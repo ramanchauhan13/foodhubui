0cf3d2116c967f3d500664c174780ac3c41510e3
ramanchauhan13 <ramanchauhan13@gmail.com>
2025-04-08 20:59:31 +0530

    dm

--- src/components/common/Content.jsx
+++ src/components/common/Content.jsx
@@ -86,7 +86,7 @@ function Content({ restaurant = [] }) {
             </h1>
           </div>
         ) : (
-          <div className="flex flex-col items-center justify-center h-30 w-30 sm:h-60 sm:w-60 cursor-pointer"
+          <div className="flex flex-col items-center justify-center h-35 w-35 sm:h-60 sm:w-60 cursor-pointer"
             onClick={() => handleSectionClick(item)}
           >
             <img
@@ -142,7 +142,7 @@ function Content({ restaurant = [] }) {
   spaceBetween={10}
   slidesPerView={3} // shows 3 per view in mobile
   breakpoints={{
-    320: { slidesPerView: 4, spaceBetween: 10 },
+    320: { slidesPerView: 3, spaceBetween: 10 },
     768: { slidesPerView: 4, spaceBetween: 20 },
     1024: { slidesPerView: 5, spaceBetween: 30 },
   }}
